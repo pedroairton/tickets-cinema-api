8d89d0f2834109b78e3cf448e39a17dc968f6f50
pedroairtonz <pedroairton@zaite.com.br>
2026-06-03 16:48:20 -0300

    Fix storage

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return dd('welcome');
+});
+Route::get('/teste-arquivo', function () {
+    return file_exists(public_path('storage/teste.txt'))
+        ? 'existe'
+        : 'nao existe';
 });
